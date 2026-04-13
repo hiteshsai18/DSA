4c0afad5c5b4567a651483ec3f103fd635084137
Hitesh Sai Emmaneni <hiteshsai.1824@gmail.com>
2026-04-13 11:48:00 +0530

    Add files via upload

--- alphabetsincrementby2.py
+++ alphabetsincrementby2.py
@@ -1,14 +1,11 @@
 #take n=hitesh and add +2 to each alphabet's number to get output as jkvguj without using ascii.
 
 alphabets = 'abcdefghijklmnopqrstuvwxyz'
-n = 'hitesh'
+n = input("Enter your name:")
 output = ''
 index_map = {char: i for i, char in enumerate(alphabets)}
 for char in n:
     index = index_map[char]
-    new_index = (index + 2) % 26
+    new_index = (index + 10) % 26
     output += alphabets[new_index]
 print(output)
-
-
-
